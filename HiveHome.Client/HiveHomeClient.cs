fbd98a3901bfa9624649b7a44600693c2b2a4e4e
Mark Leybourne <mleyb"live.co.uk>
2017-07-10 13:02:45 +0100

    Basic login/out

--- HiveHome.Client/HiveHomeClient.cs
+++ HiveHome.Client/HiveHomeClient.cs
@@ -1,4 +1,6 @@
 ﻿using HiveHome.Client.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,9 +9,13 @@ namespace HiveHome.Client
 {
     public class HiveHomeClient : IHiveHomeClient, IDisposable
     {
-        private static readonly ILog Logger = LogProvider.For<HiveHomeClient>();
+        // https://api.prod.bgchprod.info:8443/api/docs?docContext=v6&apiVersion=6.5
+
+        private static readonly ILog _logger = LogProvider.For<HiveHomeClient>();
 
         private HttpClient _client = new HttpClient();
+
+        public string SessionId { get; private set; }
 
         public HiveHomeClient()
         {
@@ -22,19 +28,48 @@ namespace HiveHome.Client
 
         public void Dispose()
         {
-            LogoutAsync().Wait();
-
-            _client.Dispose();
+            try
+            {                
+                LogoutAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                _logger.Warn(ex, "Exception on attempt to end session");
+            }
+            finally
+            {
+                _client.Dispose();
+            }
         }
 
         public async Task LoginAsync(string username, string password)
         {
-            await Task.Yield();
+            if (!String.IsNullOrEmpty(SessionId))
+                throw new InvalidOperationException("Session already exists");
+
+            JObject body = JObject.FromObject(new
+            {
+                sessions = new
+                {
+                    username = username,
+                    password = password,
+                    caller = "WEB"
+                }
+            });
+            
+            HttpResponseMessage response = await _client.PostAsync("/auth/sessions", new StringContent(body.ToString()));
+
+            // TODO - get sessionId from response & set SessionId
         }
 
         public async Task LogoutAsync()
         {
-            await Task.Yield();
+            if (!String.IsNullOrEmpty(SessionId))
+                throw new InvalidOperationException("No session exists");
+
+            await _client.DeleteAsync($"/auth/sessions/{SessionId}");
+
+            SessionId = null;
         }
     }
 }
